514be5afcc0aad8c389c4fbeed5e09d11acade22
Uday Vunnam <uday4vunnam@gmail.com>
2023-11-13 19:42:15 +0530

    feat: invoice form

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import Invoice from '~/components/invoice';
-import { Button } from '~/components/ui/button';
-import { Eye } from 'lucide-react';
+import { Invoice } from '~/components/invoice';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-slate-50">
+    <main className="flex min-h-screen flex-col items-center bg-slate-50">
       <header className="mx-auto flex items-center justify-between p-6 lg:px-8 w-full">
         <div>
           <h1 className="scroll-m-20 text-2xl font-semibold tracking-tight">Invoice Generator</h1>
--- components/invoice.tsx
+++ components/invoice.tsx
@@ -1,24 +1,23 @@
-/**
- * v0 by Vercel.
- * @see https://v0.dev/t/nLq26xe1qDD
- */
-import {
-  CardTitle,
-  CardDescription,
-  CardHeader,
-  CardContent,
-  CardFooter,
-  Card,
-} from '~/components/ui/card';
+import { Download, Eye, Plus, Trash } from 'lucide-react';
 import { Button } from '~/components/ui/button';
-import { Label } from '~/components/ui/label';
+import { Card, CardContent, CardFooter, CardHeader } from '~/components/ui/card';
 import { Input } from '~/components/ui/input';
+import { Label } from '~/components/ui/label';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '~/components/ui/select';
 import { Textarea } from '~/components/ui/textarea';
-import { Plus, Trash, Download, Eye } from 'lucide-react';
+import { currency_symbols } from '~/lib/currency';
 
-export default function Invoice() {
+const currencies = Object.keys(currency_symbols);
+
+export function Invoice() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-[minmax(900px,_1fr)_320px] gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-[minmax(900px,_1fr)_320px] gap-4 p-6 lg:px-8 ">
       <Card className="w-full max-w-4xl">
         <CardHeader>
           <div className="grid grid-cols-3 gap-4">
@@ -40,34 +39,19 @@ export default function Invoice() {
         <CardContent className="space-y-10">
           <div className="grid grid-cols-2 gap-8">
             <div className="space-y-4">
-              <div>Client Details</div>
+              <Label htmlFor="client-name">To - Client Details</Label>
               <div className="space-y-2">
-                <Label htmlFor="client-name">Client Name</Label>
                 <Input id="client-name" placeholder="Enter client's name" />
-              </div>
-              <div className="space-y-2">
-                <Label htmlFor="client-email">Client Email</Label>
                 <Input id="client-email" placeholder="Enter client's email" type="email" />
-              </div>
-              <div className="space-y-2">
-                <Label htmlFor="bill-to">Bill To Address</Label>
                 <Textarea id="bill-to" placeholder="Enter Client Address" />
               </div>
             </div>
             <div className="space-y-4">
-              <div>Biller Details</div>
+              <Label>From - Biller Details</Label>
               <div className="space-y-2">
-                <Label htmlFor="bill-from">Bill From Name</Label>
                 <Input id="bill-from" placeholder="Enter your name" />
-              </div>
-              <div className="space-y-2">
-                <Label htmlFor="biller-email">Biller Email</Label>
                 <Input id="biller-email" placeholder="Enter your email" type="email" />
-              </div>
-
-              <div className="space-y-2">
-                <Label htmlFor="biller-address">Biller Address</Label>
-                <Textarea id="biller-address" placeholder="Enter Client Address" />
+                <Textarea id="biller-address" placeholder="Enter your Address" />
               </div>
             </div>
           </div>
@@ -78,7 +62,7 @@ export default function Invoice() {
               <div className="grid grid-cols-6 gap-2">
                 <Input className="col-span-2" placeholder="Description" />
                 <Input placeholder="Qty" type="number" />
-                <Input placeholder="Unit Price (Currency Symbol)" type="number" />
+                <Input placeholder="Unit Price" type="number" />
                 <Input placeholder="Total" type="number" />
                 <div className="flex justify-end gap-2">
                   <Button className=" text-gray-500" variant="ghost" size="icon">
@@ -108,7 +92,18 @@ export default function Invoice() {
           <div className="grid grid-cols-1 gap-4">
             <div className="space-y-2">
               <Label htmlFor="currency">Currency</Label>
-              <Input id="currency" placeholder="Enter currency" />
+              <Select>
+                <SelectTrigger>
+                  <SelectValue placeholder="Select currency" />
+                </SelectTrigger>
+                <SelectContent>
+                  {currencies.map((currency) => (
+                    <SelectItem value={currency} key={currency}>
+                      {currency}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
             </div>
             <div className="space-y-2">
               <Label htmlFor="tax-rate">Tax Rate (%)</Label>
